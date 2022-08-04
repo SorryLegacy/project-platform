6e42937338f7c44f5b9589faaa825c21f82ed1ec
SorryLegacy <alexremnev2@gmail.com>
2022-08-04 16:04:26 +0300

    Create CRUD for projects

--- projects/urls.py
+++ projects/urls.py
@@ -5,4 +5,7 @@ from . import views
 urlpatterns = [
     path('', views.ProjectView.as_view(), name="projects"),
     path('project-object/<str:pk>/', views.SingleProject.as_view(), name="project"),
+    path('craete-project', views.CreateProjectView.as_view(), name="create-project"),
+    path('project-object/<str:pk>/update', views.UpdateProject.as_view(), name="update-project"),
+    path('project-object/<str:pk>/delete', views.DeleteProject.as_view(), name="delete-project"),
 ]
--- projects/views.py
+++ projects/views.py
@@ -1,5 +1,7 @@
 from .models import Project
-from django.views.generic import DetailView, ListView
+from .forms import ProjectForm
+from django.urls import reverse_lazy
+from django.views.generic import DetailView, ListView, CreateView, UpdateView, DeleteView
 
 
 class ProjectView(ListView):
@@ -13,3 +15,24 @@ class SingleProject(DetailView):
     model = Project
     template_name = 'projects/single-project.html'
 
+
+class CreateProjectView(CreateView):
+    """View for create project"""
+    model = Project
+    form_class = ProjectForm
+    template_name = 'projects/create-project.html'
+
+
+class UpdateProject(UpdateView):
+    """View for update project"""
+    model = Project
+    form_class = ProjectForm
+    template_name = 'projects/update-project.html'
+    success_url = reverse_lazy('projects')
+
+
+class DeleteProject(DeleteView):
+    """View for delete project"""
+    model = Project
+    template_name = 'projects/delete-project.html'
+    success_url = reverse_lazy('projects')
