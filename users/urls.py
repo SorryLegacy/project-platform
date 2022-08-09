79c4687bd9c4238a62fe26a351ada21d74e2ac83
SorryLegacy <alexremnev2@gmail.com>
2022-08-09 17:54:42 +0300

    Add CRUD for profiles update signals

--- users/urls.py
+++ users/urls.py
@@ -6,5 +6,10 @@ urlpatterns = [
     path('', views.Profiles.as_view(), name='profiles'),
     path('profile/<str:pk>/', views.ProfileView.as_view(), name='single-profile'),
     path('signup/', views.SignUp.as_view(), name='signup'),
-    path('account/', views.AccountView.as_view(), name='account')
+    path('account/', views.AccountView.as_view(), name='account'),
+    path('account/update/<str:pk>/', views.UpdateAccount.as_view(), name='update-account'),
+    path('account/delete/<str:pk>/', views.DeleteAccount.as_view(), name='delete-account'),
+    path('skill/create-skill/', views.CreateSkill.as_view(), name='create-skill'),
+    path('skill/update-skill/<slug:slug>/', views.UpdateSkill.as_view(), name='update-skill'),
+    path('skill/delete-skill/<slug:slug>/', views.DeleteSkill.as_view(), name='delete-skill')
 ]
--- users/views.py
+++ users/views.py
@@ -1,30 +1,67 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.urls import reverse_lazy
-from django.views.generic import DetailView, ListView, UpdateView, CreateView
+from django.views.generic import DetailView, ListView, UpdateView, CreateView, DeleteView
 from .models import User, Profile, Skill
 from .forms import UserForm, ProfileForm, SkillForm
 
 
 class Profiles(ListView):
-    """"""
+    """View to see all profiles"""
     model = Profile
     template_name = 'users/profiles.html'
 
 
 class ProfileView(DetailView):
-    """"""
+    """View to see single profile with all info"""
     model = Profile
     template_name = 'users/single-profile.html'
 
 
 class AccountView(LoginRequiredMixin, ListView):
-    """"""
+    """view for user who want to see his profile"""
     model = Profile
     template_name = 'users/account.html'
 
     def get_queryset(self):
-        """"""
         return Profile.objects.get(user=self.request.user)
+
+
+class UpdateAccount(LoginRequiredMixin, UpdateView):
+    """View for update data for UserAccount"""
+    model = Profile
+    form_class = ProfileForm
+    template_name = 'users/update-account.html'
+    success_url = reverse_lazy('account')
+
+
+class DeleteAccount(LoginRequiredMixin, DeleteView):
+    """View for Delete Account"""
+    model = Profile
+    success_url = reverse_lazy('projects')
+    template_name = 'users/delete-account.html'
+
+
+class CreateSkill(LoginRequiredMixin, CreateView):
+    """View for create skill"""
+    model = Skill
+    form_class = SkillForm
+    success_url = reverse_lazy('account')
+    template_name = 'users/create-skill.html'
+
+
+class UpdateSkill(LoginRequiredMixin, UpdateView):
+    """View for update skill"""
+    model = Skill
+    form_class = SkillForm
+    success_url = reverse_lazy('account')
+    template_name = 'users/update-skill.html'
+
+
+class DeleteSkill(LoginRequiredMixin, DeleteView):
+    """View for delete skill"""
+    model = Skill
+    success_url = reverse_lazy('account')
+    template_name = 'users/delete-skill'
 
 
 class SignUp(CreateView):
